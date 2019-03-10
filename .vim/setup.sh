851bdfb8033949bb4b33e2efc0e3d8bfbb0135b1
jiezhong qiu <jiezhong@athena.rcrai.com>
2019-03-10 03:35:37 +0000

    also setup tmux conf

--- .vim/setup.sh
+++ .vim/setup.sh
@@ -2,4 +2,5 @@ set -x
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 ln -s ~/Conf/.vim/vimrc ~/.vimrc
 ln -s ~/Conf/.vim/vimrc.bundles ~/.vimrc.bundles
+ln -s ~/Conf/.tmux.conf ~/.tmux.conf
 vim +PluginInstall +qall
